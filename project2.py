b6ef42e8599d4f78067a5db8e4394e9ec912670e
VratislavHais <vhais@localhost.localdomain>
2019-05-03 21:05:12 +0200

    snad hotovo..

--- project2.py
+++ project2.py
@@ -16,29 +16,46 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import confusion_matrix, classification_report
+from nltk import FreqDist
+import spacy
+import re
 
 def clean_document(document):
     stopwords_eng = stopwords.words('english')
-    #for doc in document:
     document_tokenized = word_tokenize(document)
     result = [word.lower() for word in document_tokenized if word not in stopwords_eng and word not in string.punctuation]
+    result = [word.replace("[^a-zA-Z#]", " ") for word in result]
+    result = [word for word in result if len(word) > 2]
     return result
 
-def stem_document(document):
-    stemmer = PorterStemmer()
-    #for doc in document:
-    result = [stemmer.stem(word) for word in document]
+def lem_document(document, tags=['NOUN', 'ADJ']):
+    result = []
+    #for sent in document:
+    doc = nlp(" ".join(document))
+    result.append([token.lemma_ for token in doc if token.pos_ in tags])
+    return result[0]
+
+def frequent_words(x, terms = 10):
+    all_words = ' '.join([text for text in x])
+    all_words = all_words.split()
+    freq_dist = FreqDist(all_words)
+    words_df = pd.DataFrame({'word': list(freq_dist.keys()), 'count': list(freq_dist.values())})
+    result = words_df.nlargest(columns="count", n = terms)
     return result
 
-#def lem_document(document, tags=['NOUN', 'ADJ']):
-#    result = []
+def unique_brands(data):
+    seen, result = set(), []
+    for item in data:
+        if item.lower() not in seen:
+            seen.add(item.lower())
+            result.append(item)
+    return result
 
-def unique_brands(brand_list):
-    return list(set(brand_list))    
-
-def text_process(text):
-    result = clean_document(text)
-    result = stem_document(result)
+def text_process(texts):
+    result = []
+    for text in texts:
+        tmp = clean_document(text)
+        result = result + lem_document(tmp)
     return result
     
 
@@ -47,40 +64,13 @@ reviews = pd.read_csv("Amazon_Unlocked_Mobile_new_test.csv")
 reviews_brands = {}
 
 for brand in unique_brands(reviews['Brand']):
+
     reviews_brands[brand] = reviews[(reviews['Brand']) == brand]['Reviews']
+nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
 
-print(reviews_brands['Alcatel'])
-"""
-X = reviews['Reviews']
-y = reviews['Rating']
-z = reviews['Brand']
-
-transformer = CountVectorizer(analyzer=text_process).fit(X)
-#X = transformer.transform(X)
-#reviews_pos = reviews[(reviews['Rating'] > 3)]
-#reviews_neg = reviews[(reviews['Rating'] < 3)]
-#reviews_neu = reviews[(reviews['Rating'] == 3)]
-
-nb = MultinomialNB()
-
-#pos_test, pos_train, pos_st_test, pos_st_train = train_test_split(reviews_pos['Reviews'], reviews_pos['Rating'], test_size=0.3, random_state=101)
-#neg_test, neg_train, neg_st_test, neg_st_train = train_test_split(reviews_neg['Reviews'], reviews_neg['Rating'], test_size=0.3, random_state=101)
-#neu_test, neu_train, neu_st_test, neu_st_train = train_test_split(reviews_neu['Reviews'], reviews_neu['Rating'], test_size=0.3, random_state=101)
-        
-test_X, train_X, test_y, train_y, test_z, train_z = train_test_split(X, y, z, test_size=0.3, random_state=101)
-train_X = transformer.transform(train_X)
-nb.fit(train_X, train_y)
-
-for i in range(0, len(test_X)):
-    sentences = transformer.transform(sent_tokenize(test_X[i]))
-    #sentences = sent_tokenize(test_X[i])
-    for sentence in sentences:
-        if (str(camera) in str(sentence)):
-            print("here")
-            print(nb.predict(sentence))        
-preds = nb.predict(test_X)
-
-print(confusion_matrix(test_y, preds))
-print('\n')
-print(classification_report(test_y, preds))
-"""
+for brand in reviews_brands.keys():    
+    print(brand + ":")
+    print(frequent_words(text_process(reviews_brands[brand])))
+    
+print("Brand independent:")
+print(frequent_words(text_process(reviews['Reviews'])))
