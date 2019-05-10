2705c9b02567734753a9ff23fe9ad30409d2c665
VratislavHais <vhais@localhost.localdomain>
2019-05-10 19:41:06 +0200

    naive bayes

--- project2.py
+++ project2.py
@@ -12,6 +12,8 @@ import string
 import pandas as pd
 from nltk import FreqDist
 import spacy
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
 
 def twograms(nouns_adj):
     for i in range(0, len(nouns_adj)):
@@ -44,19 +46,50 @@ def lem_document(document, tags=['NOUN']):
                 if adjective.pos_ == "ADJ":
                     adj.append(adjective.lemma_)
             if (len(adj) > 0):
-                result.append(token.lemma_ + "_" + adj[0])
+                result.append(adj[0] + "_" + token.lemma_)
             #else:
             #    result.append(token.lemma_)
     #result.append([(token.lemma_ + "_" + token.head.text) for token in doc if token.pos_ in tags])
     return result
 
-def frequent_words(x, terms = 10):
+def format_list(li, terms):
+    result = ""
+    for i in range(0, len(li)):
+        if (i < terms):
+            result += str(li[i][0])
+            result += "\t"
+            result += li[i][1]
+            result += "\n"
+        else:
+            break
+    return result
+
+def format_result(good, bad, terms):
+    result = "possitive:\n"
+    result += format_list(good, terms/2)
+    result += "negative:\n"
+    result += format_list(bad, terms/2)
+    return result
+
+def frequent_words(x, terms = 30):
     all_words = ' '.join([text for text in x])
     all_words = all_words.split()
     freq_dist = FreqDist(all_words)
-    words_df = pd.DataFrame({'word': list(freq_dist.keys()), 'count': list(freq_dist.values())})
-    result = words_df.nlargest(columns="count", n = terms)
-    return result
+    x = transformer.transform(word.replace("_", " ") for word in freq_dist.keys())
+    words_df = pd.DataFrame({'word': list(freq_dist.keys()), 'count': list(freq_dist.values()), 'vector': list(x)})
+    good = []
+    bad = []
+    for i in range(1, len(words_df)):
+        if (nb.predict(words_df.at[i, 'vector']) == 5):
+            good.append([words_df.at[i, 'count'], words_df.at[i, 'word'].replace(" ", "_")])
+        else:
+            bad.append([words_df.at[i, 'count'], words_df.at[i, 'word'].replace(" ", "_")])
+        #if (nb.predict(transformer.transform(df['word'])))
+    good = sorted(good, key=lambda x:x[1])
+    bad = sorted(bad, key=lambda x:x[1])
+    
+    #result = words_df.nlargest(columns="count", n = terms)
+    return format_result(good, bad, terms)
 
 def unique_brands(data):
     seen, result = set(), []
@@ -84,9 +117,16 @@ reviews = pd.read_csv("Amazon_Unlocked_Mobile_new_test.csv")
 reviews_brands = {}
 
 for brand in unique_brands(reviews['Brand']):
-
     reviews_brands[brand] = reviews[(reviews['Brand']) == brand]['Reviews']
+
 nlp = spacy.load('en_core_web_sm')
+
+nb = MultinomialNB()
+X = reviews[(reviews['Rating'] == 5) | (reviews['Rating'] == 1)]['Reviews']
+y = reviews[(reviews['Rating'] == 5) | (reviews['Rating'] == 1)]['Rating']
+
+transformer = CountVectorizer(analyzer=clean_document).fit(X)
+nb.fit(transformer.transform(X), y)
 
 for brand in reviews_brands.keys():    
     print(brand + ":")
